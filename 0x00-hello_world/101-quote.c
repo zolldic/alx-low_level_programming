ba084ea88727ff92cf1f4b47a1bf761ab39515eb
= <=>
2025-06-15 14:53:13 +0200

    fix: add the newline at teh end of the string, remove the null character

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,8 @@
 int main(void)
 {
 	char message[] = "and that piece of art is useful\" \
-- Dora Korpar, 2015-10-19";
+- Dora Korpar, 2015-10-19\n";
 
-	write(1, message, sizeof(message));
+	write(1, message, sizeof(message) - 1);
 	return (1);
 }
