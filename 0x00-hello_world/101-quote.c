2e3cdc78e19da8abea6d85be30f81f2708754010
= <=>
2025-06-15 14:33:14 +0200

    refactor: replace fprintf with write syscall

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
-#include "stdio.h"
+#include "unistd.h"
+#include "string.h"
 
 /**
 * main - Entry point
@@ -8,8 +9,9 @@
 
 int main(void)
 {
-
-	fprintf(stdout, "and that piece of art is useful\" \
-- Dora Korpar, 2015-10-19\n");
+	char message[] = "and that piece of art is useful\" \
+- Dora Korpar, 2015-10-19\n";
+	
+	write(1, message, strlen(message));
 	return (1);
 }
