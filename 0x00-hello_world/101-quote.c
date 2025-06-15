721ade7a76140bc676311207a3bd22838f1b637b
= <=>
2025-06-15 14:30:36 +0200

    feat: C program that prints a statement to stdout

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+/**
+* main - Entry point
+*
+* Return: always 1.
+*/
+
+int main(void)
+{
+
+	fprintf(stdout, "and that piece of art is useful\" \
+- Dora Korpar, 2015-10-19\n");
+	return (1);
+}
