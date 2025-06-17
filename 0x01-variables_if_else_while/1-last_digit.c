66df413e130dd2096aceab220c974c15f49d912f
= <=>
2025-06-17 07:34:46 +0200

    feat: print the last digit of the number stored in the variable n

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) on success
+ */
+
+int main(void)
+{
+	int n;
+	int last;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	last = n % 10;
+	if (last > 5)
+		printf("Last digit of %d and is %d greater than 5\n", n, last);
+	else if (last < 6 && last != 0)
+		printf("Last digit of %d and is %d and is less than 6 and not 0\n", n, last);
+	else
+		printf("Last digit of %d and is %d\n", n, last);
+
+	return (0);
+}
