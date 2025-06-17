6f75d1e07845c202d88493cd87d33de1813a3f60
= <=>
2025-06-17 10:49:07 +0200

    feat:  program that prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all single digit numbers starting from 0.
+ *
+ * Return: Always (0) on success.
+ */
+
+int main(void)
+{
+	int zero = 48;
+
+	while (zero != 58)
+	{
+		putchar(zero);
+		zero++;
+	}
+	putchar(10);
+
+	return (0);
+}
