aa822fadce86a2325bf66e2189ff6937c404adb8
= <=>
2025-06-17 07:51:42 +0200

    fix: typo in last statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	else if (last < 6 && last != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	else
-		printf("Last digit of %d and is %d\n", n, last);
+		printf("Last digit of %d is %d and is 0\n", n, last);
 
 	return (0);
 }
