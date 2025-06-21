638674af73f013be91311a32a238bb7ee7640959
= <=>
2025-06-21 09:10:33 +0200

    refactor: handle lowest number possible INT_MIN

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,10 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-*
 * print_last_digit - function that prints the last digit of a number.
 * @n: number
 * Return: the value of the last digit
-*
 */
 
 
@@ -13,10 +12,10 @@ int print_last_digit(int n)
 {
 	int last;
 
-	if (n < 0)
-		n = -(n);
-
 	last = (n % 10);
+
+	if (last < 0)
+		last = -(last);
 
 	_putchar(last + '0');
 
