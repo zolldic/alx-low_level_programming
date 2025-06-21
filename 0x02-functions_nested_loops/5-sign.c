aeeca3946caa23230d583a689b9baacaebd4bff9
= <=>
2025-06-21 07:46:46 +0200

    feat: function that prints the sign of a number.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_sign -  function that prints the sign of a number.
+* @n: number
+* Return:
+* (1) if @n greater than zero
+* (0) if @n is zero
+* (-1) if @n is less than zero
+*/
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
