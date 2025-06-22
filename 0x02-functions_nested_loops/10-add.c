e5ab4910c46a16c0331ea3b1d0507299bbc60325
= <=>
2025-06-22 08:48:39 +0200

    feat: function that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* add - function that adds two integers and returns the result.
+* @a: first number
+* @b: second number
+* Return: the result
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
