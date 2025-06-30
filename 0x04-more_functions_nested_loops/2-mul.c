4dc2b849a1b1a8c6ad5331708a63a01fc5c82a13
= <=>
2025-06-30 08:32:57 +0200

    feat: function that multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - function that multiplies two integers
+* @a: the first number
+* @b: the second number
+* Return: the result of he mulitplication
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
