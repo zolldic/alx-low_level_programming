07ded9a37493a344077b9e9b8ca5d16a88eaba36
= <=>
2025-07-20 18:46:56 +0200

    feat: a function that reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* rev_string - a function that reverses a string
+* @s: the string to reverse
+*/
+
+void rev_string(char *s)
+{
+	int i, len = 0;
+	char tmp;
+
+	while (s[len] != '\0')
+		len++;
+	len = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len - i];
+		s[len - i] = tmp;
+	}
+}
