510080b51362e67bf81107e5580c54d252f4191c
= <=>
2025-07-26 12:10:36 +0200

    feat:  function that prints every other character of a string, starting with the first character, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* puts2 - function that prints every other character of a string,
+*	starting with the first characte
+* @str: string to print
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		if ((i % 2 == 0) && (i != 0))
+			_putchar(str[i]);
+	_putchar('\n');
+}
