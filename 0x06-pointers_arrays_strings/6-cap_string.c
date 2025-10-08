5a12309f35327c4796fbd0091ce4cf79a2be55d6
= <=>
2025-10-08 15:18:46 +0200

    feat: a function that capitalizes all words of a string.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* cap_string - a function that capitalizes all words of a string.
+* @str: a string to capitalize all its words
+* Return: (str) capitalized
+*/
+
+
+char *cap_string(char *str)
+{
+	/*
+	 * \n, ' ', \t, ',', ;, '.', !, ?, ", (, ), {, }
+	 */
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		if (str[x] >= 97 && str[x] <= 122)
+		{
+			if (
+				str[x - 1] == '\n' ||
+				str[x - 1] == 32 ||
+				str[x - 1] == '.' ||
+				str[x - 1] == '\t' ||
+				str[x - 1] == ',' ||
+				str[x - 1] == ';' ||
+				str[x - 1] == '!' ||
+				str[x - 1] == '?' ||
+				str[x - 1] == '"' ||
+				str[x - 1] == '(' ||
+				str[x - 1] == ')' ||
+				str[x - 1] == '{' ||
+				str[x - 1] == '}'
+			)
+				str[x] -= 32;
+		}
+	}
+	return (str);
+}
