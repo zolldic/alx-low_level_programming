d28687433efcde27f108a4709edff17944474f0a
= <=>
2025-10-09 17:41:25 +0200

    feat:  a function that locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strstr - a function that locates a substring
+* @haystack: the string to seach for (needle) substring
+* @needle: a substring to match
+* Return: a pointer to the beginning of the located substring
+*	or NULL if the substring is not found.
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+
+	int x, y;
+	char *ptr = NULL;
+
+
+	for (x = 0; haystack[x] != '\0'; x++)
+	{
+		if (haystack[x] == needle[0])
+		{
+			for (y = 1; needle[y] != '\0'; y++)
+				if (haystack[(x + y)] == needle[y])
+				{
+					ptr = &haystack[x];
+				}
+				else
+				{
+					ptr = NULL;
+					break;
+				}
+		}
+	}
+	return (ptr);
+}
