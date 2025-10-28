cf8702f0d489a969a06956e85dcfb09e43a6c8ae
zolldic <ojiip95@gmail.com>
2025-10-28 20:59:07 +0200

    feat: a function that frees a 2 dimensional

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+/**
+* free_grid - a function that frees a 2 dimensional
+* @grid: a 2D array previously created by alloc_grid function
+* @height: rows in the 2D array
+*/
+
+void free_grid(int **grid, int height)
+{
+
+	int x;
+
+	for (x = 0; x < height; x++)
+	  free(grid[x]);
+
+	free(grid);
+}
