6a0e58c71953e52d6b5e8538c9d7971e9af174b6
zolldic <ojiip95@gmail.com>
2025-10-28 17:29:33 +0200

    feat: a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+/**
+* _strdup - function that returns a pointer to
+*	a newly allocated space in memory
+* @str: a string to be copied to the newly allocated memory
+* Return: a pointer to the duplicated string or NULL on failure
+*/
+
+char *_strdup(char *str)
+{
+	int x, len = 0;
+	char *duplicate;
+
+	if (!str)
+		return (NULL);
+
+	for (x = 0; str[x] != '\0'; x++)
+		len++;
+
+	duplicate = (char *) malloc(sizeof(char) * len);
+
+	if (!duplicate)
+		return (NULL);
+
+	for (x = 0; x < len; x++)
+		duplicate[x] = str[x];
+
+	duplicate[x] = '\0';
+
+	return (duplicate);
+}
