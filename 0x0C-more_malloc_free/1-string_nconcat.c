ec9a379c0d7bdaf94df5a23e158cbf70f97b5d26
zolldic <ojiip95@gmail.com>
2025-10-29 17:03:23 +0200

    feat: handle when either of the strings are NULL

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 
 /**
-* _strlen - calculate string length
-* @str: string to calculate its length
-* Return: Always return the string length
-*/
+ * _strlen - calculate string length
+ * @str: string to calculate its length
+ * Return: Always return the string length
+ */
 int _strlen(char *str)
 {
 	int x = 0;
@@ -19,35 +19,41 @@ int _strlen(char *str)
 }
 
 /**
-* string_nconcat - a function that concatenates two strings
-* @s1: the first string
-* @s2: the second string
-* @n: the first bytes of s2
-* Return: a pointer to the newly concatenated string
-*	or NULL on failure
-*/
+ * string_nconcat - a function that concatenates two strings
+ * @s1: the first string
+ * @s2: the second string
+ * @n: the first bytes of s2
+ * Return: a pointer to the newly concatenated string
+ *	or NULL on failure
+ */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int len1, len2, total;
-	unsigned int x, y;
+	unsigned int x = 0, y = 0;
 	char *new;
 
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
+	
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	if (n >= len2)
-		total = len1 + len2 + 1;
-	else
-		total = len1 + n + 1;
+		n = len2;
 
-	new = (char *) malloc(sizeof(char *) * total);
+	total = len1 + n;
+	new = (char *)malloc(sizeof(char) * (total + 1));
+
 	if (!new)
 		return (NULL);
 
-	for (x = 0; x < len1; x++)
+	for (; x < len1; x++)
 		new[x] = s1[x];
-	for (y = 0; y < n; y++)
+
+	for (; y < (total - len1); y++)
 		new[x + y] = s2[y];
 
 	new[x + y] = '\0';
