c27b8f908d3c47cdbd6cfb9c4e652d5334aae141
zolldic <ojiip95@gmail.com>
2025-10-31 14:01:06 +0200

    fix: Adds parentheses to fix order of operation

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	ptr = (int *) malloc(sizeof(int) * max - min + 1);
+	ptr = (int *) malloc(sizeof(int) * (max - min + 1));
 	if (!ptr)
 		return (NULL);
 
