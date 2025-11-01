169ca10237a17d3c2300e1d1f27333025ca339b7
zolldic <ojiip95@gmail.com>
2025-11-01 18:26:09 +0200

    refactor: change the name of the dog struct to dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,14 @@
  *
  * Description: this struct holds information about dogs names, ages and owners
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 #endif
