f371c61b5f42f275517f27aef43ed94b30ed204a
zolldic <ojiip95@gmail.com>
2025-11-01 18:11:14 +0200

    feat: handle edge case where the struct is null

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,10 +5,13 @@
 * @d: dog struct
 * @name: name of the dog
 * @age: age of the dog
-* #owner: owner of the dog
+* @owner: owner of the dog
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (!d)
+		return;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
