4ee6540bcb076124ece2c26386381b4d8443960a
zolldic <ojiip95@gmail.com>
2025-11-03 22:38:29 +0200

    feat: Adds a struct of type list

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,12 @@
 
 #include <stdarg.h>
 
+typedef struct list
+{
+	char name;
+	void (*f)(va_list args);
+} list_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
