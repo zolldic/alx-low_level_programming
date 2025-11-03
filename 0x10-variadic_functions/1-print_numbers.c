7ec4f1022ed31c548e028f38cf6b57bfc0afc3b1
zolldic <ojiip95@gmail.com>
2025-11-03 20:12:59 +0200

    feat: a function that prints numbers, followed by a new line

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* print_numbers - a function that prints numbers
+* @separator: a string to be printed between numbers
+* @n: the number of integers passed to the function
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int x;
+	va_list arguments;
+
+	if (!separator)
+		separator = "";
+	va_start(arguments, n);
+
+	for (x = 0; x < n; x++)
+		x < (n - 1) ?
+			printf("%d%s", va_arg(arguments, int), separator) :
+			printf("%d\n", va_arg(arguments, int));
+
+	va_end(arguments);
+}
