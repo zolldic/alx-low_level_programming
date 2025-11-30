8887bfe8ab289823dd27ebed9067d1c47fada14c
zolldic <ojiip95@gmail.com>
2025-11-30 14:33:16 +0200

    feat: Implement a function that free a list memory and set head to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* free_listint2 - frees a listint_t list and set head to NULL
+* @head: pointer to the head of the listint_t list
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while(*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;	
+	}
+
+	head = NULL;
+}
