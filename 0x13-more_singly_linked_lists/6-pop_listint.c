a4329b59bee0598e696cecaacb6474f367e9db06
zolldic <ojiip95@gmail.com>
2025-11-30 14:40:56 +0200

    feat: Implement a function that remove the head node from a list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* pop_listint - delete the head node of a listint_t list
+* @head: pointer to the head node of listint_t list
+* Return: the head node data (n);
+*/
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int n;
+
+	n = (*head)->n;
+
+	tmp = (*head)->next;
+
+	*head = tmp;
+
+	return (n);
+}
