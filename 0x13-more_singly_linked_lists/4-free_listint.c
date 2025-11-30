1bd87a236c483a1b1f173f11ec604378d2fee9e8
zolldic <ojiip95@gmail.com>
2025-11-30 14:27:45 +0200

    feat: Implement a function that frees a list memory

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+* free_listint - free a listint_t list
+* @head: pointer to the head of listint_t list
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
